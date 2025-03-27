fc1ef12ea77eba81c54b0140145bb82cd62d83ba
cacharri <echarri94@gmail.com>
2025-03-27 18:54:10 +0100

    test

--- test_infrastructure.sh
+++ test_infrastructure.sh
@@ -7,7 +7,7 @@ echo "Esperando que los servicios estén listos..."
 sleep 10
 
 echo "Probar conexión a MariaDB desde WordPress..."
-docker exec srcs-wordpress-1 mysql -h mariadb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "SHOW DATABASES;"
+docker exec srcs_wordpress_1 mysql -h mariadb -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "SHOW DATABASES;"
 
 echo "Probar conexión de Nginx a WordPress..."
-docker exec srcs-nginx-1 curl -I http://wordpress:9000
+docker exec srcs_nginx_1 curl -I http://wordpress:9000
